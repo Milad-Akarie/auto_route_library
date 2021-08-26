c957303fedbcbb05d4286448af410909d93ebe8d
Nolence <rex.magana@gmail.com>
2021-08-26 13:55:06 -0600

    Added back proper exports

--- auto_route/lib/auto_route.dart
+++ auto_route/lib/auto_route.dart
@@ -1,15 +1,19 @@
 library auto_route;
 
-export 'src/navigation_failure.dart';
 export 'src/auto_route_guard.dart';
 export 'src/common/common.dart';
 export 'src/matcher/route_match.dart';
+export 'src/navigation_failure.dart';
 export 'src/route/page_route_info.dart';
 export 'src/route/route_config.dart';
 export 'src/router/auto_route_page.dart';
 export 'src/router/auto_router_x.dart';
+export 'src/router/controller/controller_scope.dart';
 export 'src/router/controller/routing_controller.dart';
+export 'src/router/parser/route_information_parser.dart';
+export 'src/router/widgets/auto_back_button.dart';
 export 'src/router/widgets/auto_router.dart';
 export 'src/router/widgets/auto_router.dart';
 export 'src/router/widgets/auto_router_delegate.dart';
 export 'src/router/widgets/auto_tabs_router.dart';
+export 'src/router/widgets/auto_tabs_scaffold.dart';
