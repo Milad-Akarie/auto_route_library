8a1e6476c653d361d2fd0a358c8c268bb1d6d548
Charles Dyason <k4gcybex@gmail.com>
2024-01-10 14:01:42 +0200

    feat: added printStack on `NavigatorObserver`

--- auto_route/example/lib/web_demo/router/app_navigator_observer.dart
+++ auto_route/example/lib/web_demo/router/app_navigator_observer.dart
@@ -11,21 +11,25 @@ class AppNavigatorObserver extends AutoRouterObserver {
   @override
   void didPush(Route route, Route? previousRoute) {
     print('didPush ${route.settings.name}');
+    _printStack();
   }
 
   @override
   void didPop(Route route, Route? previousRoute) {
     print('didPop ${route.settings.name}');
+    _printStack();
   }
 
   @override
   void didRemove(Route route, Route? previousRoute) {
     print('didRemove ${route.settings.name}');
+    _printStack();
   }
 
   @override
   void didReplace({Route? newRoute, Route? oldRoute}) {
     print('didReplace ${newRoute?.settings.name}');
+    _printStack();
   }
 
   void _printStack() {
