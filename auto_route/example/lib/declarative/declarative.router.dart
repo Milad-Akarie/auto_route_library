aa13db9ad1a02d5589638ba89521553d228ed3ab
milad <milad.akarie@gmail.com>
2024-07-30 22:35:21 +0300

    add declarative navigation example

---
+++ auto_route/example/lib/declarative/declarative.router.dart
@@ -0,0 +1,218 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+import 'declarative.router.gr.dart';
+
+void main() {
+  runApp(DeclarativeNavigationExampleApp());
+}
+
+@AutoRouterConfig(generateForDir: ['lib/declarative'])
+class DecRouter extends RootStackRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(
+          page: MainRoute.page,
+          initial: true,
+          children: [
+            AutoRoute(page: NameInputRoute.page),
+            AutoRoute(page: AgeInputRoute.page),
+            AutoRoute(page: ResultRoute.page),
+          ],
+        ),
+      ];
+
+
+}
+
+class DeclarativeNavigationExampleApp extends StatelessWidget {
+  final _router = DecRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router.config(),
+    );
+  }
+}
+
+@RoutePage()
+class MainScreen extends StatefulWidget {
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final _profileNotifier = ValueNotifier<Profile>(const Profile());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Declarative Navigation Example')),
+      backgroundColor: Colors.grey[200],
+      body: ValueListenableBuilder(
+        valueListenable: _profileNotifier,
+        builder: (context, profile, _) {
+          return AutoRouter.declarative(
+            routes: (_) {
+              return [
+                if (profile.name == null)
+                  NameInputRoute(
+                    onNameSubmitted: (name) {
+                      _profileNotifier.value = profile.copyWith(name: name);
+                    },
+                  ),
+                if (profile.name != null && profile.age == null)
+                  AgeInputRoute(
+                    onAgeSubmitted: (age) {
+                      _profileNotifier.value = profile.copyWith(age: age);
+                    },
+                  ),
+                if (profile.name != null && profile.age != null)
+                  ResultRoute(
+                    profile: profile,
+                    onReset: () {
+                      _profileNotifier.value = const Profile();
+                    },
+                  ),
+              ];
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+@RoutePage()
+class NameInputScreen extends StatefulWidget {
+  final ValueChanged<String> onNameSubmitted;
+
+  const NameInputScreen({Key? key, required this.onNameSubmitted}) : super(key: key);
+
+  @override
+  State<NameInputScreen> createState() => _NameInputScreenState();
+}
+
+class _NameInputScreenState extends State<NameInputScreen> {
+  final _controller = TextEditingController();
+
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: Colors.white,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Enter your name'),
+              TextField(controller: _controller,textAlign: TextAlign.center),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  widget.onNameSubmitted(_controller.text);
+                },
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// age submit page
+@RoutePage()
+class AgeInputScreen extends StatefulWidget {
+  final ValueChanged<int> onAgeSubmitted;
+
+  const AgeInputScreen({Key? key, required this.onAgeSubmitted}) : super(key: key);
+
+  @override
+  State<AgeInputScreen> createState() => _AgeInputScreenState();
+}
+
+class _AgeInputScreenState extends State<AgeInputScreen> {
+  final _controller = TextEditingController();
+
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+        color: Colors.white,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Enter your age'),
+              TextField(controller: _controller,textAlign: TextAlign.center),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  widget.onAgeSubmitted(int.parse(_controller.text));
+                },
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+@RoutePage()
+class ResultScreen extends StatelessWidget {
+  final Profile profile;
+  final VoidCallback onReset;
+
+  const ResultScreen({Key? key, required this.profile, required this.onReset}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+        color: Colors.white,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Name: ${profile.name}'),
+              Text('Age: ${profile.age}'),
+              SizedBox(height: 16),
+              ElevatedButton(onPressed: onReset, child: Text('Reset State')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Profile {
+  const Profile({this.name, this.age});
+
+  final String? name;
+  final int? age;
+
+  Profile copyWith({String? name, int? age}) {
+    return Profile(
+      name: name ?? this.name,
+      age: age ?? this.age,
+    );
+  }
+}
---
+++ auto_route/example/lib/declarative/declarative.router.gr.dart
@@ -0,0 +1,176 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:example/declarative/declarative.router.dart' as _i1;
+import 'package:flutter/material.dart' as _i3;
+
+/// generated route for
+/// [_i1.AgeInputScreen]
+class AgeInputRoute extends _i2.PageRouteInfo<AgeInputRouteArgs> {
+  AgeInputRoute({
+    _i3.Key? key,
+    required _i3.ValueChanged<int> onAgeSubmitted,
+    List<_i2.PageRouteInfo>? children,
+  }) : super(
+          AgeInputRoute.name,
+          args: AgeInputRouteArgs(
+            key: key,
+            onAgeSubmitted: onAgeSubmitted,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'AgeInputRoute';
+
+  static _i2.PageInfo page = _i2.PageInfo(
+    name,
+    builder: (data) {
+      final args = data.argsAs<AgeInputRouteArgs>();
+      return _i1.AgeInputScreen(
+        key: args.key,
+        onAgeSubmitted: args.onAgeSubmitted,
+      );
+    },
+  );
+}
+
+class AgeInputRouteArgs {
+  const AgeInputRouteArgs({
+    this.key,
+    required this.onAgeSubmitted,
+  });
+
+  final _i3.Key? key;
+
+  final _i3.ValueChanged<int> onAgeSubmitted;
+
+  @override
+  String toString() {
+    return 'AgeInputRouteArgs{key: $key, onAgeSubmitted: $onAgeSubmitted}';
+  }
+}
+
+/// generated route for
+/// [_i1.MainScreen]
+class MainRoute extends _i2.PageRouteInfo<void> {
+  const MainRoute({List<_i2.PageRouteInfo>? children})
+      : super(
+          MainRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'MainRoute';
+
+  static _i2.PageInfo page = _i2.PageInfo(
+    name,
+    builder: (data) {
+      return _i1.MainScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [_i1.NameInputScreen]
+class NameInputRoute extends _i2.PageRouteInfo<NameInputRouteArgs> {
+  NameInputRoute({
+    _i3.Key? key,
+    required _i3.ValueChanged<String> onNameSubmitted,
+    List<_i2.PageRouteInfo>? children,
+  }) : super(
+          NameInputRoute.name,
+          args: NameInputRouteArgs(
+            key: key,
+            onNameSubmitted: onNameSubmitted,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'NameInputRoute';
+
+  static _i2.PageInfo page = _i2.PageInfo(
+    name,
+    builder: (data) {
+      final args = data.argsAs<NameInputRouteArgs>();
+      return _i1.NameInputScreen(
+        key: args.key,
+        onNameSubmitted: args.onNameSubmitted,
+      );
+    },
+  );
+}
+
+class NameInputRouteArgs {
+  const NameInputRouteArgs({
+    this.key,
+    required this.onNameSubmitted,
+  });
+
+  final _i3.Key? key;
+
+  final _i3.ValueChanged<String> onNameSubmitted;
+
+  @override
+  String toString() {
+    return 'NameInputRouteArgs{key: $key, onNameSubmitted: $onNameSubmitted}';
+  }
+}
+
+/// generated route for
+/// [_i1.ResultScreen]
+class ResultRoute extends _i2.PageRouteInfo<ResultRouteArgs> {
+  ResultRoute({
+    _i3.Key? key,
+    required _i1.Profile profile,
+    required _i3.VoidCallback onReset,
+    List<_i2.PageRouteInfo>? children,
+  }) : super(
+          ResultRoute.name,
+          args: ResultRouteArgs(
+            key: key,
+            profile: profile,
+            onReset: onReset,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'ResultRoute';
+
+  static _i2.PageInfo page = _i2.PageInfo(
+    name,
+    builder: (data) {
+      final args = data.argsAs<ResultRouteArgs>();
+      return _i1.ResultScreen(
+        key: args.key,
+        profile: args.profile,
+        onReset: args.onReset,
+      );
+    },
+  );
+}
+
+class ResultRouteArgs {
+  const ResultRouteArgs({
+    this.key,
+    required this.profile,
+    required this.onReset,
+  });
+
+  final _i3.Key? key;
+
+  final _i1.Profile profile;
+
+  final _i3.VoidCallback onReset;
+
+  @override
+  String toString() {
+    return 'ResultRouteArgs{key: $key, profile: $profile, onReset: $onReset}';
+  }
+}
